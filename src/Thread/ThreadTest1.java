058956a35a6a78717a46412eb0b767948d684d51
renhai1 <1457481392@qq.com>
2021-08-30 10:28:52 +0800

    多线程入门

--- src/Thread/ThreadTest1.java
+++ src/Thread/ThreadTest1.java
@@ -10,10 +10,10 @@ public class ThreadTest1 {
     public static void main(String[] args) {
         MyTheadTest1 myTheadTest1 = new MyTheadTest1();
         myTheadTest1.start();
+//        myTheadTest1.run();
         for (int i = 0; i < 100; i++) {
-            if(i%2!=0)
-            {
-                System.out.println(i+"main");
+            if (i % 2 != 0) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
             }
         }
     }
@@ -21,18 +21,19 @@ public class ThreadTest1 {
 }
 
 /**
- * @Author  renhai
- * @Description  线程创建
+ * @Author renhai
+ * @Description 线程创建
  * @Date 9:12 2021/8/30
  * @Param
  * @return
  **/
+
 class MyTheadTest1 extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
-                System.out.println(i);
+                System.out.println(Thread.currentThread().getName() + ":" + i);
             }
         }
     }
