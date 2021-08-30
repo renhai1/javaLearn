dee944866e73aaa5aa58a15fbf00fb1115c629d9
renhai1 <1457481392@qq.com>
2021-08-30 10:11:23 +0800

    多线程入门例子

---
+++ src/Thread/ThreadTest1.java
@@ -0,0 +1,39 @@
+package Thread;
+
+/**
+ * @className: ThreadTest1
+ * @description: java学习
+ * @author: renhai
+ * @create: 2021-08-30 9:09
+ */
+public class ThreadTest1 {
+    public static void main(String[] args) {
+        MyTheadTest1 myTheadTest1 = new MyTheadTest1();
+        myTheadTest1.start();
+        for (int i = 0; i < 100; i++) {
+            if(i%2!=0)
+            {
+                System.out.println(i+"main");
+            }
+        }
+    }
+
+}
+
+/**
+ * @Author  renhai
+ * @Description  线程创建
+ * @Date 9:12 2021/8/30
+ * @Param
+ * @return
+ **/
+class MyTheadTest1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
