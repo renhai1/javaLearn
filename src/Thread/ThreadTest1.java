8ea195dbd8dab43671a5b29ce04c4a3758044bb8
renhai1 <1457481392@qq.com>
2021-08-30 11:15:17 +0800

    多线程入门

--- src/Thread/ThreadTest1.java
+++ src/Thread/ThreadTest1.java
@@ -9,13 +9,24 @@ package Thread;
 public class ThreadTest1 {
     public static void main(String[] args) {
         MyTheadTest1 myTheadTest1 = new MyTheadTest1();
+        myTheadTest1.setName("线程一");
         myTheadTest1.start();
 //        myTheadTest1.run();
+        Thread.currentThread().setName("主线程：");
         for (int i = 0; i < 100; i++) {
             if (i % 2 != 0) {
                 System.out.println(Thread.currentThread().getName() + ":" + i);
             }
         }
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + ":******");
+
+                }
+            }
+        }.start();
     }
 
 }
@@ -34,7 +45,12 @@ class MyTheadTest1 extends Thread {
         for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(Thread.currentThread().getName() + ":" + i);
+//                if (i%20==0)
+//                {
+//                   this.yield();
+//                }
             }
+
         }
     }
 }
