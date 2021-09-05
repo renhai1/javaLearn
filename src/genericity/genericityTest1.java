3863f945f2d7cf5fa10cc18b8281994ab0d3db93
renhai1 <1457481392@qq.com>
2021-09-05 19:59:05 +0800

    泛型的讲解

--- src/genericity/genericityTest1.java
+++ src/genericity/genericityTest1.java
@@ -14,6 +14,14 @@ public class genericityTest1 {
     @Test
     public void test1() {
         //泛型不能是基本类型
+        /**
+         * A是B的父类  G<B>不能赋值给G<A>
+         * G继承与T  G<A>可以赋值给T<A>
+         *对于list<?>不能添加东西，只能添加null
+         * list<?>赋值之后可以读取数据
+         * G<? extends A> 可以作为G<A> 和G<B>的父类的，其中B是A的子类
+         * G<? super A> 可以作为G<A> 和G<B>的父类的，其中B是A的父类
+         */
         ArrayList<String> list1 = new ArrayList<String>();
         list1.add("renhai");
         list1.add("Sherry");
