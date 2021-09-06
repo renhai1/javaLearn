dd0a524573069054dcfaa067ee77c6a0abc2f343
renhai1 <1457481392@qq.com>
2021-09-06 16:35:29 +0800

    调用运行时类指定的结构

--- src/reflect/reflect3.java
+++ src/reflect/reflect3.java
@@ -8,10 +8,20 @@ package reflect;
  */
 public class reflect3 {
     /**
+     *  想要改变private权限修饰的东西 需要设置权限
      * Field getDeclaredField(String name)
      * 返回一个 Field对象，它反映此表示的类或接口的指定已声明字段 类对象。
      *
      * Field getDeclaredField(String name)
      * 返回一个 Field对象，它反映此表示的类或接口的指定已声明字段 类对象。
+     *
+     * 方法 getDeclaredMethod(String name, 类<?>... parameterTypes)
+     * 返回一个 方法对象，它反映此表示的类或接口的指定声明的方法 类对象。
+     *
+     * 方法 getMethod(String name, 类<?>... parameterTypes)
+     * 返回一个 方法对象，它反映此表示的类或接口的指定公共成员方法 类对象。
+     *
+     * invoke(class,method)调用反射得到的类实例对象的方法
+     *
      */
 }
