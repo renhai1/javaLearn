1224d0f93e80806ba3571f26b0397a0b3cc72788
renhai1 <1457481392@qq.com>
2021-09-06 16:22:32 +0800

    调用运行时类指定的结构

--- src/reflect/reflect3.java
+++ src/reflect/reflect3.java
@@ -7,4 +7,11 @@ package reflect;
  * @create: 2021-09-06 16:18
  */
 public class reflect3 {
+    /**
+     * Field getDeclaredField(String name)
+     * 返回一个 Field对象，它反映此表示的类或接口的指定已声明字段 类对象。
+     *
+     * Field getDeclaredField(String name)
+     * 返回一个 Field对象，它反映此表示的类或接口的指定已声明字段 类对象。
+     */
 }
