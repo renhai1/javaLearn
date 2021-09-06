06ba994088942eef44c37369206872062ade3272
renhai1 <1457481392@qq.com>
2021-09-06 16:02:06 +0800

    获得运行时类的完整结构

---
+++ src/reflect/reflect2.java
@@ -0,0 +1,17 @@
+package reflect;
+
+import org.junit.Test;
+
+/**
+ * @className:reflect2
+ * @description: 获得运行时类的完整结构
+ * @author: renhai
+ * @create: 2021-09-06 16:01
+ */
+public class reflect2 {
+    @Test
+    public void test1()
+    {
+
+    }
+}
