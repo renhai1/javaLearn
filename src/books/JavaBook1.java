851e6bad658343a7c4f17968863c620f249306f2
renhai1 <1457481392@qq.com>
2021-09-10 11:04:15 +0800

    java书习题练习

---
+++ src/books/JavaBook1.java
@@ -0,0 +1,23 @@
+package books;
+
+import org.junit.Test;
+
+import static java.lang.Math.*;
+
+/**
+ * @className:JavaBook1
+ * @description: java书基础卷练习
+ * @author: renhai
+ * @create: 2021-09-09 21:32
+ */
+public class JavaBook1 {
+    @Test
+    public void test1()
+    {
+        double num1=2.0-1.1;
+        System.out.println(2.0-1.1);
+        System.out.println(num1);
+        System.out.println(PI);
+        System.out.println(E);
+    }
+}
