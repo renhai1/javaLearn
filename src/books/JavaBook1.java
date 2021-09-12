7bc8fea4c676a684efe1880d09192afc05823d82
renhai1 <1457481392@qq.com>
2021-09-12 16:49:39 +0800

    equals方法测试

--- src/books/JavaBook1.java
+++ src/books/JavaBook1.java
@@ -2,6 +2,8 @@ package books;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static java.lang.Math.*;
 
 /**
@@ -12,12 +14,32 @@ import static java.lang.Math.*;
  */
 public class JavaBook1 {
     @Test
-    public void test1()
+    public void test1() {
+        double num1 = 2.0 - 1.1;
+        System.out.println(2.0 - 1.1);
+        System.out.printf("%.3f",num1);
+
+//        System.out.println(PI);
+//        System.out.println(E);
+//        System.out.println(4 <= 5);
+    }
+    @Test
+    public void test2()
     {
-        double num1=2.0-1.1;
-        System.out.println(2.0-1.1);
-        System.out.println(num1);
-        System.out.println(PI);
-        System.out.println(E);
+        String str1="renhai";
+        String str2=new String("renhai");
+        boolean flag=(str1==str2);
+        boolean flag1=(str1.equals(str2));
+        System.out.println(flag);//false
+        System.out.println(flag1);//true
+    }
+    @Test
+    public void test3()
+    {
+        String str1=new String("renhai");
+        String str2=new String("renhai");
+        String str3="renhai";
+        System.out.println(str1.equals(str2));
+
     }
 }
