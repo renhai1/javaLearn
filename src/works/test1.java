72280c968b89c4a2849c2600e3899565190f847f
renhai1 <1457481392@qq.com>
2021-09-13 10:10:25 +0800

    包的讲解

---
+++ src/works/test1.java
@@ -0,0 +1,17 @@
+package works;
+
+import org.junit.Test;
+
+/**
+ * @className:test1
+ * @description:
+ * @author: renhai
+ * @create: 2021-09-13 10:09
+ */
+public class test1 {
+    @Test
+    public  void test1()
+    {
+        //包的讲解
+    }
+}
