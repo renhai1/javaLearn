a9c301b1bbd47ab90c0e26fcf4f6811d1b9f97dd
renhai1 <1457481392@qq.com>
2021-09-20 17:21:02 +0800

    java第三章面向对象作业测试代码

--- src/works/test1.java
+++ src/works/test1.java
@@ -2,6 +2,8 @@ package works;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 /**
  * @className:test1
  * @description: java网课练习
@@ -10,11 +12,21 @@ import org.junit.Test;
  */
 public class test1 {
     @Test
-    public  void test1()
-    {
+    public void test1() {
         round round1 = new round(1);
         System.out.println(round1.getLength());
         System.out.println(round1.getSquare());
+        BankAccount bankAccount1 = new BankAccount("1457481392");
+        bankAccount1.deposit(500);
+        bankAccount1.withdrawal(100);
+        System.out.println(bankAccount1.getBalance());
+        employee employee1 = new employee();
+        employee1.setName("Sherry");
+        employee1.setSex('女');
+        employee1.setDate(new Date());
+        employee1.setSalary(500);
+        System.out.println(employee1);
+
 
     }
 }
